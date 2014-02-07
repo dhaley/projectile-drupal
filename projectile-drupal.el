fb7a61b5172260599b9c1d432e46cf5d0dc94d44
Damon Haley <damon.haley@colorado.edu>
2014-02-07 16:02:56 -0700

    changes + keybinding

--- projectile-drupal.el
+++ projectile-drupal.el
@@ -44,102 +44,6 @@
   :prefix "projectile-drupal-"
   :group 'projectile)
 
-(defcustom projectile-drupal-controller-keywords
-  '("logger" "polymorphic_path" "polymorphic_url" "mail" "render" "attachments"
-    "default" "helper" "helper_attr" "helper_method" "layout" "url_for"
-    "serialize" "exempt_from_layout" "filter_parameter_logging" "hide_action"
-    "cache_sweeper" "protect_from_forgery" "caches_page" "cache_page"
-    "caches_action" "expire_page" "expire_action" "rescue_from" "params"
-    "request" "response" "session" "flash" "head" "redirect_to"
-    "render_to_string" "respond_with" "before_filter" "append_before_filter"
-    "prepend_before_filter" "after_filter" "append_after_filter"
-    "prepend_after_filter" "around_filter" "append_around_filter"
-    "prepend_around_filter" "skip_before_filter" "skip_after_filter" "skip_filter")
-  "List of keywords to highlight for controllers"
-  :group 'projectile-drupal
-  :type '(repeat string))
-
-(defcustom projectile-drupal-migration-keywords
-  '("create_table" "change_table" "drop_table" "rename_table" "add_column"
-    "rename_column" "change_column" "change_column_default" "remove_column"
-    "add_index" "remove_index" "rename_index" "execute")
-  "List of keywords to highlight for migrations"
-  :group 'projectile-drupal
-  :type '(repeat string))
-
-(defcustom projectile-drupal-model-keywords
-  '("default_scope" "named_scope" "scope" "serialize" "belongs_to" "has_one"
-    "has_many" "has_and_belongs_to_many" "composed_of" "accepts_nested_attributes_for"
-    "before_create" "before_destroy" "before_save" "before_update" "before_validation"
-    "before_validation_on_create" "before_validation_on_update" "after_create"
-    "after_destroy" "after_save" "after_update" "after_validation"
-    "after_validation_on_create" "after_validation_on_update" "around_create"
-    "around_destroy" "around_save" "around_update" "after_commit" "after_find"
-    "after_initialize" "after_rollback" "after_touch" "attr_accessible"
-    "attr_protected" "attr_readonly" "validates" "validate" "validate_on_create"
-    "validate_on_update" "validates_acceptance_of" "validates_associated"
-    "validates_confirmation_of" "validates_each" "validates_exclusion_of"
-    "validates_format_of" "validates_inclusion_of" "validates_length_of"
-    "validates_numericality_of" "validates_presence_of" "validates_size_of"
-    "validates_existence_of" "validates_uniqueness_of" "validates_with")
-  "List of keywords to highlight for models"
-  :group 'projectile-drupal
-  :type '(repeat string))
-
-(defcustom projectile-drupal-view-keywords
-  '("action_name" "atom_feed" "audio_path" "audio_tag" "auto_discovery_link_tag"
-    "button_tag" "button_to" "button_to_function" "cache" "capture" "cdata_section"
-    "check_box" "check_box_tag" "collection_select" "concat" "content_for"
-    "content_tag" "content_tag_for" "controller" "controller_name"
-    "controller_path" "convert_to_model" "cookies" "csrf_meta_tag" "csrf_meta_tags"
-    "current_cycle" "cycle" "date_select" "datetime_select" "debug"
-    "distance_of_time_in_words" "distance_of_time_in_words_to_now" "div_for"
-    "dom_class" "dom_id" "email_field" "email_field_tag" "escape_javascript"
-    "escape_once" "excerpt" "favicon_link_tag" "field_set_tag" "fields_for"
-    "file_field" "file_field_tag" "flash" "form_for" "form_tag"
-    "grouped_collection_select" "grouped_options_for_select" "headers"
-    "hidden_field" "hidden_field_tag" "highlight" "image_alt" "image_path"
-    "image_submit_tag" "image_tag" "j" "javascript_cdata_section"
-    "javascript_include_tag" "javascript_path" "javascript_tag" "l" "label"
-    "label_tag" "link_to" "link_to_function" "link_to_if" "link_to_unless"
-    "link_to_unless_current" "localize" "logger" "mail_to" "number_field"
-    "number_field_tag" "number_to_currency" "number_to_human" "number_to_human_size"
-    "number_to_percentage" "number_to_phone" "number_with_delimiter"
-    "number_with_precision" "option_groups_from_collection_for_select"
-    "options_for_select" "options_from_collection_for_select" "params"
-    "password_field" "password_field_tag" "path_to_audio" "path_to_image"
-    "path_to_javascript" "path_to_stylesheet" "path_to_video" "phone_field"
-    "phone_field_tag" "pluralize" "provide" "radio_button" "radio_button_tag"
-    "range_field" "range_field_tag" "raw" "render" "request"
-    "request_forgery_protection_token" "reset_cycle" "response" "safe_concat"
-    "safe_join" "sanitize" "sanitize_css" "search_field" "search_field_tag"
-    "select" "select_date" "select_datetime" "select_day" "select_hour"
-    "select_minute" "select_month" "select_second" "select_tag" "select_time"
-    "select_year" "session" "simple_format" "strip_links" "strip_tags"
-    "stylesheet_link_tag" "stylesheet_path" "submit_tag" "t" "tag" "telephone_field"
-    "telephone_field_tag" "text_area" "text_area_tag" "text_field" "text_field_tag"
-    "time_ago_in_words" "time_select" "time_tag" "time_zone_options_for_select"
-    "time_zone_select" "translate" "truncate" "url_field" "url_field_tag"
-    "url_for" "url_options" "video_path" "video_tag" "word_wrap")
-  "List of keywords to highlight for views"
-  :group 'projectile-drupal
-  :type '(repeat string))
-
-(defcustom projectile-drupal-active-support-keywords
-  '("alias_attribute" "with_options" "delegate")
-  "List of keywords to highlight for all `projectile-drupal-mode' buffers"
-  :group 'projectile-drupal
-  :type '(repeat string))
-
-(defcustom projectile-drupal-font-lock-face-name 'font-lock-keyword-face
-  "Face to be used for higlighting drupal the keywords")
-
-(defcustom projectile-drupal-views-re
-  "\\.\\(?:html\\|erb\\|haml\\|js\\|slim\\|json\\|coffee\\|css\\)$"
-  "Regexp for filtering for view files"
-  :group 'projectile-drupal
-  :type 'string)
-
 (defcustom projectile-drupal-errors-re
   "\\([0-9A-Za-z@_./\:-]+\\.rb\\):?\\([0-9]+\\)?"
   "The regex used to find errors with file paths."
@@ -164,7 +68,7 @@
 (defcustom projectile-drupal-add-keywords t
   "If not nil the drupal keywords will be font locked in the mode's bufffers.")
 
-(defcustom projectile-drupal-keymap-prefix (kbd "C-c r")
+(defcustom projectile-drupal-keymap-prefix (kbd "C-8")
   "`projectile-drupal-mode' keymap prefix."
   :group 'projectile-drupal
   :type 'string)
@@ -207,24 +111,6 @@ The binded variables are \"singular\" and \"plural\"."
 (defun projectile-drupal-zeus-p ()
   (file-exists-p (projectile-expand-root ".zeus.sock")))
 
-(defun projectile-drupal--highlight-keywords (keywords)
-  "Highlight the passed KEYWORDS in current buffer."
-  (font-lock-add-keywords
-   nil
-   (list (list
-          (concat "\\(^\\|[^_:.@$]\\|\\.\\.\\)\\b"
-                  (regexp-opt keywords t)
-                  "\\_>")
-          (list 2 projectile-drupal-font-lock-face-name)))))
-
-(defun projectile-drupal-add-keywords-for-file-type ()
-  "Apply extra font lock keywords specific to models, controllers etc."
-  (loop for (re keywords) in `(("_controller\\.rb$"   ,projectile-drupal-controller-keywords)
-                               ("app/models/.+\\.rb$" ,projectile-drupal-model-keywords)
-                               ("db/migrate/.+\\.rb$" ,projectile-drupal-migration-keywords))
-        do (when (and (buffer-file-name) (string-match-p re (buffer-file-name)))
-             (projectile-drupal--highlight-keywords
-              (append keywords projectile-drupal-active-support-keywords)))))
 
 (defun projectile-drupal-choices (dirs)
   "Uses `projectile-dir-files' function to find files in directories.
@@ -373,18 +259,6 @@ Returns a hash table with keys being short names and values being relative paths
   (setq-local auto-revert-verbose nil)
   (buffer-disable-undo)
   (projectile-drupal-on))
-
-(defun projectile-drupal-rake-tmp-file ()
-  (projectile-expand-root "tmp/rake-output"))
-
-(defun projectile-drupal-rake-tasks ()
-  "Returns a content of tmp file with rake tasks."
-  (if (file-exists-p (projectile-drupal-rake-tmp-file))
-      (with-temp-buffer
-        (insert-file-contents (projectile-drupal-rake-tmp-file))
-        (buffer-string))
-    (projectile-drupal-regenerate-rake)
-    (projectile-drupal-rake-tasks)))
 
 (defun projectile-drupal-root ()
   "Returns drupal root directory if this file is a part of a Drupal application else nil"
@@ -696,7 +570,6 @@ If file does not exist and ASK in not nil it will ask user to proceed."
       (define-key prefix-map (kbd "@") 'projectile-drupal-find-mailer)
       (define-key prefix-map (kbd "y") 'projectile-drupal-find-layout)
       (define-key prefix-map (kbd "r") 'projectile-drupal-console)
-      (define-key prefix-map (kbd "k") 'projectile-drupal-rake)
       (define-key prefix-map (kbd "t") 'projectile-drupal-generate)
       (define-key prefix-map (kbd "RET") 'projectile-drupal-goto-file-at-point)
 
@@ -737,7 +610,6 @@ If file does not exist and ASK in not nil it will ask user to proceed."
     ["Go to current migration"   projectile-drupal-find-current-migration]
     "--"
     ["Run console"               projectile-drupal-console]
-    ["Run rake"                  projectile-drupal-rake]
     ["Run drupal generate"       projectile-drupal-generate]))
 
 ;;;###autoload
@@ -747,7 +619,6 @@ If file does not exist and ASK in not nil it will ask user to proceed."
   :lighter " Drupal"
   (when projectile-drupal-mode
     (and projectile-drupal-expand-snippet (projectile-drupal-expand-snippet-maybe))
-    (and projectile-drupal-add-keywords (projectile-drupal-add-keywords-for-file-type))
     (projectile-drupal-set-javascript-dirs)))
 
 ;;;###autoload
